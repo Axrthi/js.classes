d930700ed82dab2cc8030bbf03532836159b3048
Axrthi <an2006324@gmail.com>
2025-07-31 10:43:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const obj={
+    firstname:"Aarthi",
+    lastname:"N",
+    age:19,
+    graduated:false,
+    joining_date:new Date("11/08/2025"),
+    number_of_days:()=>{
+        let curr=new Date()
+        return curr-obj.joining_date
+    }
+}
+console.log(obj)
+
+
+
+let{firstname,lastname,age,graduated,joining_date,number_of_days}=obj
+console.log(
+    Math.floor(number_of_days()/(1000*3600*24))
+)
+
+
