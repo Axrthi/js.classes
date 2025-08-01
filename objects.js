32947b3e820be6556e3e5c6b9bddb5b4fbcad79b
Axrthi <an2006324@gmail.com>
2025-08-01 15:32:00 +0530

    Update and rename script.js to objects.js

--- objects.js
+++ objects.js
@@ -17,19 +17,19 @@ console.log(obj2.firstname)
 
 //forEach
 const arr=[5,3,6,9,12,14,16,345,56,346]
-//arr.forEach((x)=>{console.log(x)})
+arr.forEach((x)=>{console.log(x)})
 
 //filter
-//let filteredArr=arr.filter((x)=>x%2==0)
-//console.log(filteredArr)
+let filteredArr=arr.filter((x)=>x%2==0)
+console.log(filteredArr)
 
 //reduce
-//let sum=arr.reduce((acc,val)=>{ if(val%2!=0)return acc+val; return acc})
-//console.log(sum)
+let sum=arr.reduce((acc,val)=>{ if(val%2!=0)return acc+val; return acc})
+console.log(sum)
 
 //slice
-//let copyArr=arr.slice(2,4)
-//console.log(copyArr)
+let copyArr=arr.slice(2,4)
+console.log(copyArr)
 
 //splice
 arr.splice(2,4)
